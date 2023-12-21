89de4a3b4fdb81e4f8d85e2f39812fbac48a45be
Gregory Bouteiller <gregory.bouteiller@niama.re>
2023-12-21 17:57:21 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			images: {
+				domains: [],
+				formats: ["image/avif", "image/webp"],
+				sizes: [256, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+			},
+		})
 	}
 };
 
