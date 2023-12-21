6ddaed840db2e66f0c1c7268ed589a7a4e1fba98
Gregory Bouteiller <gregory.bouteiller@niama.re>
2023-12-21 18:51:21 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,29 @@ import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess({})],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			images: {
-				domains: [],
-				formats: ["image/avif", "image/webp"],
-				sizes: [256, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-			},
-		})
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      images: {
+        domains: [],
+        formats: ['image/avif', 'image/webp'],
+        sizes: [256, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '^prod-files-secure.s3.us-west-2.amazonaws.com$',
+          },
+        ],
+      },
+    }),
+  },
 };
 
 export default config;
+
