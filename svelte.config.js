bde41c0f8beee6cc1686f38815a1d657a3e454e5
Gregory Bouteiller <gregory.bouteiller@niama.re>
2023-12-21 21:10:03 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,15 +13,9 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
       images: {
-        domains: [],
+        domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
         formats: ['image/avif', 'image/webp'],
         sizes: [256, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '^prod-files-secure\\.s3\\.us-west-2\\.amazonaws\\.com$',
-          },
-        ],
       },
     }),
   },
