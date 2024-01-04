3a13f6e64a6acfa7633917d695b39ca03e1a2542
Gregory Bouteiller <gregory.bouteiller@niama.re>
2024-01-04 11:20:25 +0700

    Update index.ts

--- src/lib/notion/server/index.ts
+++ src/lib/notion/server/index.ts
@@ -7,7 +7,7 @@ import {createNotionAssets, createNotionCms} from '@niama/notion-tools/server';
 export const {findEntry, findEntries, zIdToDataEntry, zIdsToDataEntries, zSlugsToContentEntries} = createNotionCms({
   auth: NOTION_TOOLS_KEY,
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-  fetch: (input, fetchOptions) => eFetch(input, {duration: '59m', type: 'json', fetchOptions}),
+  fetch: (input, fetchOptions) => eFetch(input, {duration: '59m', dryRun: !dev, type: 'json', fetchOptions}),
   // fetch: async (input, fetchOptions) => {
   //   const response = await fetch(input, fetchOptions);
   //   return response.json();
